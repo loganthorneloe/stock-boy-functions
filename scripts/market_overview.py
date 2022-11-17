044a3adc6ed5b5c6aab6007be9c4403a46a0a285
Logan Thorneloe <loganthorneloe@macbook-pro.lan>
2022-11-17 15:26:46 -0600

    Updating total fundamentals.

---
+++ scripts/market_overview.py
@@ -0,0 +1,12 @@
+
+# average market confidence over time
+
+# current average confidence
+
+# histogram of confidence in 10% chunks at the current moment
+
+# high scores table
+
+# historical high scores
+
+# no red or black fundamentals
